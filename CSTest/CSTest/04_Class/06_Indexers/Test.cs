d2300e323b730015bf780ea2df62a42be18743dc
AndreA <someone@nowhere.com>
2015-10-26 08:38:37 +0200

    Update

--- CSTest/CSTest/04_Class/06_Indexers/Test.cs
+++ CSTest/CSTest/04_Class/06_Indexers/Test.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
 
 namespace CSTest._04_Class._06_Indexers
 {
@@ -8,6 +9,19 @@ namespace CSTest._04_Class._06_Indexers
         [TestMethod]
         public void TestIndexer1()
         {
+            TestClass my = new TestClass();
+
+            my[0] = 1;
+            my[1] = 2;
+            my[2] = 3;
+            my[3] = 4;
+            my[4] = 5;
+
+            Debug.WriteLine(my[0]);
+            Debug.WriteLine(my[1]);
+            Debug.WriteLine(my[2]);
+            Debug.WriteLine(my[3]);
+            Debug.WriteLine(my[4]);
         }
     }
 }
---
+++ CSTest/CSTest/09_Array/0_Setup/TestClass.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace CSTest._09_Array._0_Setup
+{
+    public interface IAnimal
+    {
+        void Voice();
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Voice()
+        {
+            Debug.WriteLine("Gav-Gav");
+        }
+
+        public void Jump()
+        {
+            Debug.WriteLine("Jump");
+        }
+    }
+}
