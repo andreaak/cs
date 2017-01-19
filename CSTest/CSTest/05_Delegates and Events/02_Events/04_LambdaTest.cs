d99b79cf1487544edb46cd4feb0a4da8b05dc7f9
andrea <you@example.com>
2017-01-19 21:49:48 +0200

    Update

--- CSTest/CSTest/05_Delegates and Events/02_Events/04_LambdaTest.cs
+++ CSTest/CSTest/05_Delegates and Events/02_Events/04_LambdaTest.cs
@@ -61,6 +61,58 @@ namespace CSTest._05_Delegates_and_Events._02_Events
             */
         }
 
+        [Test]
+        public void TestEventsLambda3()
+        {
+            LambdaEvent obj = new LambdaEvent();
+            int index = 5;
+            LambdaEventHandler h = (i) => SomeMethod(index, i);
+            obj.SomeEvent += h;
+
+            LambdaEvent obj2 = new LambdaEvent();
+            int index2 = 4;
+            LambdaEventHandler h2 = (i) => SomeMethod(index2, i);
+            obj2.SomeEvent += h2;
+
+
+
+            obj.SomeEvent -= h;
+            obj2.SomeEvent -= h2;
+
+
+            LambdaEvent2[] objs = new LambdaEvent2[2];
+            objs[0] = new LambdaEvent2();
+            objs[1] = new LambdaEvent2();
+
+            Action<int>[] acts = new Action<int>[2];
+            for (int i = 0; i < 2; i++)
+            {
+                int index3 = i;
+                acts[i] = (j) => SomeMethod(index3, j);
+                objs[i].SomeEvent += acts[i];
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                objs[i].OnSomeEvent(i + 9);
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                objs[i].SomeEvent -= acts[i];
+            }
+
+            /*
+            Index: 0. Var: 9
+            Index: 1. Var: 10
+            */
+        }
+
+        private void SomeMethod(int index, int i)
+        {
+            Debug.WriteLine("Index: {0}. Var: {1}", index, i);
+        }
+
         delegate void LambdaEventHandler(int n);
         // Объявить класс, содержащий событие, 
         class LambdaEvent
@@ -74,6 +126,17 @@ namespace CSTest._05_Delegates_and_Events._02_Events
             }
         }
 
+        // Объявить класс, содержащий событие, 
+        class LambdaEvent2
+        {
+            public event Action<int> SomeEvent;
+            // Этот метод вызывается для запуска события, 
+            public void OnSomeEvent(int n)
+            {
+                if (SomeEvent != null)
+                    SomeEvent(n);
+            }
+        }
 
         // Пример обработки событий, связанных с нажатием клавиш на клавиатуре. 
         // Создать класс, производный от класса EventArgs и 
@@ -99,6 +162,5 @@ namespace CSTest._05_Delegates_and_Events._02_Events
                 }
             }
         }
-
     }
 }
