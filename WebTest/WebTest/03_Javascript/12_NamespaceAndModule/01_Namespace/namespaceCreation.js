965bca67ccdd152f7610cb6ac38e156f1d4e9244
ankulikov <ankulikov@playtika.com>
2017-06-07 17:21:01 +0300

    Update JS

---
+++ WebTest/WebTest/03_Javascript/12_NamespaceAndModule/01_Namespace/namespaceCreation.js
@@ -0,0 +1,23 @@
+﻿var App = App || {};
+
+App.define = function (namespace) {
+    var parts = namespace.split("."),
+        parent = App,
+        i;
+
+    // убрать начальный префикс если это имя глобальной переменной
+    if (parts[0] === "App") {
+        parts = parts.slice(1); // ['utils', 'dom']
+    }
+
+    // если в глобальном объекте нет свойства - создать его.
+    for (i = 0; i < parts.length; i++) {
+
+        if (typeof parent[parts[i]] == "undefined") {
+            parent[parts[i]] = {};
+        }
+
+        parent = parent[parts[i]];
+    }
+    return parent;
+}
---
+++ WebTest/WebTest/03_Javascript/12_NamespaceAndModule/02_Module/moduleTemlate.js
@@ -0,0 +1,22 @@
+﻿// определяем пространство имен
+App.define("util.calc");
+
+// Определение модуля
+// инициализируем объект используя немелденно вызываемую функцию.
+App.util.calc = (function () {
+    // закрытые члены
+    var x, y;
+
+    return {
+        // открытые члены
+        add: function () {
+            return x + y;
+        },
+        setX: function (val) {
+            x = val;
+        },
+        setY: function (val) {
+            y = val;
+        }
+    }
+}());
