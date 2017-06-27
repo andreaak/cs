f5181c5d8b0fb5b7e745518ca6cfa4e27a460d13
ankulikov <ankulikov@playtika.com>
2017-06-27 17:32:34 +0300

    Update HTML

--- WebTest/WebTest/03_Javascript/01_Introduction/01_ScriptLoading/script.js
+++ WebTest/WebTest/03_Javascript/01_Introduction/01_ScriptLoading/script.js
@@ -1,11 +1,2 @@
-var a = 5;
-var b = 5;
-var theNumbersMatch
-
-if ( a == b ) {
-    theNumbersMatch = true;
-} else {
-    theNumbersMatch = false;
-}
-
-console.log(theNumbersMatch);
+var res = document.getElementById("result");
+res.innerHTML = "Hello!";
