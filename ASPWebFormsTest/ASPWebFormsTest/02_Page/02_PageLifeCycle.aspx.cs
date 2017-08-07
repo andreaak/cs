08aba2b820d0d9ec2eb421deee2e327086b8ba55
ankulikov <ankulikov@playtika.com>
2017-08-07 19:12:35 +0300

    Update ASP

--- ASPWebFormsTest/ASPWebFormsTest/02_Page/02_PageLifeCycle.aspx.cs
+++ ASPWebFormsTest/ASPWebFormsTest/02_Page/02_PageLifeCycle.aspx.cs
@@ -7,8 +7,11 @@ namespace ASPWebFormsTest._02_Page
     {
         private StringBuilder _output = new StringBuilder();
 
+        // Событие используется для динамической установки MasterPage, смены темы.
         protected void Page_PreInit(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_PreInit"));
+
             _output.Clear();
             _output.Append($"<p><span class=\"bold\">IsPostBack: {IsPostBack}</span></p>");
             _output.Append("<p><span class=\"bold\">Page_PreInit</span><br/>");
@@ -16,15 +19,22 @@ namespace ASPWebFormsTest._02_Page
             _output.Append("</p>");
         }
 
+        // Для чтения или инициализации свойств элементов управления.
         protected void Page_Init(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_Init"));
+
             _output.Append("<p><span class=\"bold\">Page_Init</span><br/>");
             _output.Append($"TextBox1: {TextBox1.Text}");
             _output.Append("</p>");
         }
 
+        // Для действий, которые должны произвестись после всех инициализаций. 
+        // До этого момента данные сохраненные во ViewState не сериализуються.
         protected void Page_InitComplete(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_InitComplete"));
+
             // no sense. ViewState not loaded  
             var test1 = ViewState["Test1"] as string;
             var test2 = ViewState["Test2"] as string;
@@ -46,8 +56,11 @@ namespace ASPWebFormsTest._02_Page
         //{
         //}
 
+        // Для выполнения действий после загрузки ViewState и до события Load.
         protected void Page_PreLoad(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_PreLoad"));
+
             var test1 = ViewState["Test1"] as string;
             var test2 = ViewState["Test2"] as string;
             var test3 = ViewState["Test3"] as string;
@@ -59,8 +72,11 @@ namespace ASPWebFormsTest._02_Page
             _output.Append("</p>");
         }
 
+        // Используется для установки и инициализаций свойств открытия подключений к базам данных.
         protected void Page_Load(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_Load"));
+
             _output.Append("<p><span class=\"bold\">Page_Load<br/>");
             _output.Append($"TextBox1: {TextBox1.Text}");
             _output.Append("</p>");
@@ -80,27 +96,38 @@ namespace ASPWebFormsTest._02_Page
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            Response.Write(PrepareEmphasizedMessage("Button1_Click"));
+
             _output.Append("<p><span class=\"bold\">Button1_Click</span><br/>");
             _output.Append($"TextBox1: {TextBox1.Text}");
             _output.Append("</p>");
         }
 
+        // Для действий, которые должны происходить после выполнения обработчиков событий элементов управления.
         private void Page_LoadComplete(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_LoadComplete"));
+
             _output.Append("<p><span class=\"bold\">Page_LoadComplete</span><br/>");
             _output.Append($"TextBox1: {TextBox1.Text}");
             _output.Append("</p>");
         }
 
+        // Для внесения окончательных изменений перед рендерингом страницы.
         protected void Page_PreRender(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_PreRender"));
+
             _output.Append("<p><span class=\"bold\">Page_PreRender</span><br/>");
             _output.Append($"TextBox1: {TextBox1.Text}");
             _output.Append("</p>");
         }
 
+        // Используется при разработке асинхронных страниц.
         protected void Page_PreRenderComplete(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_PreRenderComplete"));
+
             ViewState["Test1"] = "ViewState_Page_PreRenderComplete";
             ViewState["Test3"] = "ViewState_Page_PreRenderComplete_3";
             _output.Append("<p><span class=\"bold\">Page_PreRenderComplete</span><br/>");
@@ -112,14 +139,17 @@ namespace ASPWebFormsTest._02_Page
         //{
         //}
 
+        // Сохранение ViewState. Последнее событие перед рендерингом.
         protected void Page_SaveStateComplete(object sender, EventArgs e)
         {
+            Response.Write(PrepareMessage("Page_SaveStateComplete"));
+
             ViewState["Test2"] = "ViewState_Page_SaveStateComplete"; // no sense. View state is saved
             _output.Append("<p><span class=\"bold\">Page_SaveStateComplete</span><br/>");
             _output.Append($"TextBox1: {TextBox1.Text}<br/>");
             _output.Append("</p>");
 
-            Output.Text = _output.ToString();
+            //Output.Text = _output.ToString();
         }
 
         //protected void Page_Render(object sender, EventArgs e)
@@ -136,6 +166,18 @@ namespace ASPWebFormsTest._02_Page
         protected void Page_Disposed(object sender, EventArgs e)
         {
         }
+
+        #region Help methods
+        private string PrepareMessage(string message)
+        {
+            return string.Format("<div style='background-color:lightgreen; border:1px solid black; padding:12px;margin:4px;'>{0}</div>", message);
+        }
+
+        private string PrepareEmphasizedMessage(string message)
+        {
+            return string.Format("<div style='background-color:purple; border:1px solid black; padding:12px;margin:4px;'>{0}</div>", message);
+        }
+        #endregion
 
         /* 
         IsPostBack: False
--- ASPWebFormsTest/ASPWebFormsTest/Global.asax.cs
+++ ASPWebFormsTest/ASPWebFormsTest/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Web;
 using System.Web.Routing;
 
@@ -6,27 +7,117 @@ namespace ASPWebFormsTest
 {
     public class Global : HttpApplication
     {
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Обработчики событий приложения.
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public static string name = null;
         // Обработчик события старта приложения. Срабатывает один раз при первом запросе к веб приложению.
         void Application_Start(object sender, EventArgs e)
         {
+            Debug.WriteLine("Start");
+            name = "ghhh" + DateTime.Now.ToString();
             // RouteTable.Routes - коллекция всех маршрутов текущего приложения.
             RegisterRoutes(RouteTable.Routes);
         }
 
+        // Вызывается при завершении приложения. 
+        // Приложение завершается, если к нему, в течении 20 минут (по умолчанию), не было ни одного запроса.
         void Application_End(object sender, EventArgs e)
         {
-            //  Code that runs on application shutdown
-
+            Debug.WriteLine("End");
         }
 
+        // В приложении возникло исключение, которое не обработали
         void Application_Error(object sender, EventArgs e)
         {
-            // Code that runs when an unhandled error occurs
+            Debug.WriteLine("Error");
+            // Получение исключения, которое произошло на странице.
+            Exception ex = Server.GetLastError().GetBaseException();
+        }
 
+        // Срабатывает, когда начинается новая сессия.
+        void Session_Start(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Session_Start");
+        }
+
+        // Срабатывает при завершении сессии.
+        // Этот метод будет вызываться только если в web.config, в секции sessionState, атрибут mode установлен, как InProc
+        // InProc - это значение по умолчанию, означает, что данные сеанса будут храниться в текущем рабочем процессе.
+        void Session_End(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Session_End");
+        }
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Обработчики событий запроса. (В хронологическом порядке.)
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Вызывается при каждом запросе.
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("BeginRequest");
+            Debug.WriteLine(name);
+        }
+
+        // Срабатывает после аутентификации пользователя, выполняющего запрос. (аутентификация - определение принадлежности к ресурсу, поиск учетной записи)
+        void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("AuthenticateRequest");
+        }
+
+        // Срабатывает после авторизации пользователя, выполняющего запрос. (авторизация - определение наличия прав доступа)
+        void Application_AuthorizeRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("AuthorizeRequest");
+        }
+
+        // Модули отвечающие за кэширование, извлекают ранее обработанный запрос, минуя вызов обработчика события.
+        void Application_ResolveRequestCache(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ResolveRequestCache");
+        }
+
+        // Срабатывает после получения состояния сессии (Session state) для текущего запроса.
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            Debug.WriteLine("AcquireRequestState");
+        }
+
+        // Срабатывает до того как начнется вызов обработчика.
+        protected void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            Debug.WriteLine("PreRequestHandlerExecute");
+        }
+
+        // ВЫЗОВ ОБРАБОТЧИКА.
+
+        // Срабатывает после завершения обработчика события.
+        protected void Application_PostRequestHandlerExecute(object sender, EventArgs e)
+        {
+            Debug.WriteLine("PostRequestHandlerExecute");
+        }
+
+        // Вызывается тогда, когда информация сеанса сериализуется из коллекции Session, чтобы стать доступной в следующем запросе
+        protected void Application_ReleaseRequestState(object sender, EventArgs e)
+        {
+            Debug.WriteLine("ReleaseRequestState");
+        }
+
+        // Вызывается перед добавлением информации в кэш выходных данных, если он был разрешен
+        protected void Application_UpdateRequestCache(object sender, EventArgs e)
+        {
+            Debug.WriteLine("UpdateRequestCache");
+        }
+
+        // Последнее событие, которое происходит при обработке запроса.
+        protected void Application_EndRequest(object sender, EventArgs e)
+        {
+            Debug.WriteLine("EndRequest");
         }
 
         // Метод для регистрации маршрутов.
-        void RegisterRoutes(RouteCollection routes)
+        private void RegisterRoutes(RouteCollection routes)
         {
             /*
             Маршрут для запросов brosers/ie, browsers/сhrome и т.д.
