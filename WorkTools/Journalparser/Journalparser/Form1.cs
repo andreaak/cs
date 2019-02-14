c243dda06ab247e5dc67ef8cbb6a9a620bcf0e0a
Andrey Kulikov <andrey.kulikov@autodesk.com>
2019-02-14 13:23:08 +0200

    Init journal parser

---
+++ WorkTools/Journalparser/Journalparser/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Journalparser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonParse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFileDlg = new OpenFileDialog();
+            oFileDlg.Filter = "TXT Files (*.txt)|*.txt|" + "All Files (*.*)|*.*";
+            if (oFileDlg.ShowDialog() == DialogResult.OK && oFileDlg.FileName.Length > 0)
+            {
+                string file = oFileDlg.FileName;
+                var points = Parser.Parse(file, (int)numericUpDown1.Value);
+                WriteToFile(points);
+                MessageBox.Show("Done");
+            }
+
+        }
+
+        private void WriteToFile(List<FreezedItem> points)
+        {
+            using (var writer = File.CreateText("result.txt"))
+            {
+                foreach (var item in points)
+                {
+                    writer.WriteLine($"Duration: {item.Time} Start: {item.Start} Line Start: {item.LineStart} End: {item.End} Line End: {item.LineEnd}");
+                    writer.WriteLine("");
+                    writer.WriteLine(item.Text);
+                    writer.WriteLine("\r\n");
+                }
+            }
+        }
+    }
+}
---
+++ WorkTools/Journalparser/Journalparser/Parser.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journalparser
+{
+    public class DateItem
+    {
+        public DateTime Date;
+        public int Line;
+    }
+
+    public class FreezedItem
+    {
+        public DateTime Start;
+        public int LineStart;
+        public DateTime End;
+        public int LineEnd;
+        public int Time;
+        public string Text;
+    }
+
+    public class Parser
+    {
+        public static List<FreezedItem> points;
+
+        public static List<FreezedItem> Parse(string file, int minDuration)
+        {
+            points = new List<FreezedItem>();
+            int index = 1;
+            using (StreamReader rd = File.OpenText(file))
+            {
+                string line = null;
+                var strings = new List<string>();
+                DateItem prevItem = null;
+                while ((line = rd.ReadLine()) != null)
+                {
+                    var item = ParseLine(line, index++);
+                    if(item != null)
+                    {
+                        if (prevItem != null && IsFiltered(minDuration, prevItem, item))
+                        {
+                            strings.Add(line);
+                            points.Add(new FreezedItem {
+
+                                Start = prevItem.Date,
+                                LineStart = prevItem.Line,
+                                End = item.Date,
+                                LineEnd = item.Line,
+                                Time = (int)((item.Date - prevItem.Date).TotalSeconds),
+                                Text = string.Join("\n", strings)
+                            });
+                        }
+
+                        //
+                        prevItem = item;
+                        strings.Clear();
+                        strings.Add(line);
+                    }
+                    else
+                    {
+                        strings.Add(line);
+                    }
+                }
+            }
+            return points;
+        }
+
+        private static bool IsFiltered(int minDuration, DateItem prevItem, DateItem item)
+        {
+            var diff = GetInterval(prevItem, item);
+            return diff >= minDuration;
+        }
+
+        private static int GetInterval(DateItem prevItem, DateItem item)
+        {
+            return (int)((item.Date - prevItem.Date).TotalSeconds);
+        }
+
+        private static DateItem ParseLine(string line, int index)
+        {
+            var items = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            if(items.Length == 0)
+            {
+                return null;
+            }
+            string str = (items.ElementAtOrDefault(1) + " " + items.ElementAtOrDefault(2)).TrimEnd(';');
+            return DateTime.TryParse(str, out DateTime time) && items[0].StartsWith("'")?
+                            new DateItem { Date = time, Line = index }:
+                            null;
+        }
+
+
+    }
+}
